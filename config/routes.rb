6a05f85bf0a66b57d8ca10581f4f7ece955e1515
Zain Fathoni <me@zainf.dev>
2024-10-06 01:47:18 +0700

    Set the Application Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "articles#index"
 
   get "/articles", to: "articles#index"
 end
