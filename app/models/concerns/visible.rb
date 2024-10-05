0a54334a1ea8636fca676944bfe1eed04ee8da0a
Zain Fathoni <me@zainf.dev>
2024-10-06 03:29:53 +0700

    Implement Visible concerns for Articles and Comments

---
+++ app/models/concerns/visible.rb
@@ -0,0 +1,23 @@
+module Visible
+  extend ActiveSupport::Concern
+
+  VALID_STATUSES = [ "public", "private", "archived" ]
+
+  included do
+    validates :status, inclusion: { in: VALID_STATUSES }
+  end
+
+  class_methods do
+    def public_count
+      where(status: "public").count
+    end
+  end
+
+  def public?
+    status == "public"
+  end
+
+  def archived?
+    status == "archived"
+  end
+end
