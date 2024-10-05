ac81b2ec260d12c4bc9c208a67989c30b3c2a8c1
Zain Fathoni <me@zainf.dev>
2024-10-06 03:35:33 +0700

    Implement Basic Authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "zain", password: "fathoni", except: [ :index, :show ]
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "zain", password: "fathoni", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
