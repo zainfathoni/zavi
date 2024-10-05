942916cbb4af59b405669a4b180b6e0b48cce8ba
Zain Fathoni <me@zainf.dev>
2024-10-06 03:05:17 +0700

    Adding a Route for Comments

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
