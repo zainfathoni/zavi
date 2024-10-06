d6383fbcb2facd4fa4d213f3c793fd3d176d1ae0
Zain Fathoni <me@zainf.dev>
2024-10-06 09:30:51 +0700

    Style the Comment partial and Article

--- app/helpers/tailwind_form_builder.rb
+++ app/helpers/tailwind_form_builder.rb
@@ -16,7 +16,7 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def submit(options = {})
-    options[:class] ||= "cursor-pointer rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    options[:class] ||= "rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     super
   end
 
@@ -29,7 +29,7 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
       )
       @template.concat(
         select(method, choices, options, html_options.merge(
-          class: "mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          class: "mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6 cursor-pointer"
         ))
       )
     end
